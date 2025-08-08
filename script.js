bfd47a1e7a71e56553a41d2966ab0251db229213
enfieldr <jaganarul179@gmail.com>
2025-08-08 11:33:12 +0530

    updated

--- script.js
+++ script.js
@@ -139,3 +139,113 @@ function botReply(msg) {
     // ...Scroll to Section on Request...
 }
 // ...existing code...
+
+let tl = gsap.timeline({
+	// yes, we can add it to an entire timeline!
+	scrollTrigger: {
+		trigger: '.container',
+		pin: true, // pin the trigger element while active
+		start: 'top top', // when the top of the trigger hits the top of the viewport
+		end: '+=500', // end after scrolling 500px beyond the start
+		scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+		snap: {
+			snapTo: 'labels', // snap to the closest label in the timeline
+			duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+			delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+			ease: 'power1.inOut' // the ease of the snap animation ("power3" by default)
+		}
+	}
+});
+
+// add animations and labels to the timeline
+tl.addLabel('start')
+	.from('.box p', { scale: 0.3, rotation: 45, autoAlpha: 0 })
+	.addLabel('color')
+	.from('.box', { backgroundColor: '#28a92b' })
+	.addLabel('spin')
+	.to('.box', { rotation: 360 })
+	.addLabel('end');
+
+  
+
+  let tween = gsap.to(".class", {
+  x: 100,
+  id: "example",
+  scrollTrigger: ".trigger",
+});
+
+console.log(ScrollTrigger.getById("example").animation);
+
+// ==== clock ====|
+
+const INPUT = document.querySelector('[type=range]')
+
+const ROTATE_MAPPER = gsap.utils.mapRange(0, INPUT.max, 0, 360)
+const PROGRESS_MAPPER = gsap.utils.mapRange(0, INPUT.max * 0.5, 0, 50)
+
+gsap.registerPlugin(Draggable)
+
+gsap.set('[title="moon"]', { scale: 0 })
+
+const minToTime = minutes => `${`${Math.floor(minutes/60)}`.padStart(2, '0')}:${`${Math.floor(minutes%60)}`.padStart(2, '0')}`
+
+
+const SUNRISE = 465
+const SUNSET = 465 + (12 * 60)
+// let rotationSnap = 360 / 24
+let rotationSnap = 15
+
+const onInput = function() {
+  if (this.rotation) {
+    let VALUE = Math.floor(((this.rotation % 360) / 360) * INPUT.max)
+    if (VALUE < 0) VALUE = INPUT.max - Math.abs(VALUE)
+    INPUT.value = VALUE
+  } else {
+    gsap.set('.radial__ring', {
+      rotate: ROTATE_MAPPER(INPUT.value)
+    }) 
+  }
+  const TIME = minToTime(gsap.utils.wrap(0, 1440, parseInt(INPUT.value, 10) + (SUNSET + (SUNRISE - SUNSET) * 0.5)))
+  gsap.set('.radial__time', {
+    innerText: TIME
+  })
+  gsap.set('.radial__handle', {
+    rotate: ROTATE_MAPPER(-INPUT.value)
+  })
+  if (INPUT.value > INPUT.max * 0.25 && INPUT.value < INPUT.max * 0.75) {
+    gsap.to('[title="sun"]', { scale: 0, duration: 0.25 })
+    gsap.to('[title="moon"]', { scale: 1, duration: 0.25 })
+    gsap.set('.radial__handle', { background: 'var(--gray-8)' })
+    gsap.set('body', { background: 'var(--text-1)'})
+    gsap.set('.radial__time', { color: 'var(--surface-1)'})
+  } else {
+    gsap.to('[title="moon"]', { scale: 0, duration: 0.25 })
+    gsap.to('[title="sun"]', { scale: 1, duration: 0.25 })
+    gsap.set('.radial__handle', { background: 'var(--gray-0)' })
+    gsap.set('body', { background: 'var(--surface-1)' })
+    gsap.set('.radial__time', { color: 'var(--text-1)' })
+  }
+  // Based on the value work out the conic-gradient percentages...
+  // Remember the ring starts at 0 and the input at 0 but the stops are offset.
+  const sunInput = INPUT.value > INPUT.max * 0.75 ? parseInt(INPUT.value, 10) - INPUT.max * 0.75 : parseInt(INPUT.value, 10) + INPUT.max * 0.25
+  const moonInput = INPUT.value > INPUT.max * 0.75 ? 0 : INPUT.value - INPUT.max * 0.25
+  gsap.set('.radial__track', {
+    '--sun-stop': gsap.utils.clamp(0, 50, PROGRESS_MAPPER(sunInput)),
+    '--moon-stop': gsap.utils.clamp(0, 50, PROGRESS_MAPPER(moonInput))
+  })
+}
+
+window.__RADIAL = Draggable.create(".radial__ring", {
+  trigger: '.radial__handle',
+  type:"rotation",
+  inertia: true,
+  onThrowUpdate: onInput,
+  onDrag: onInput,
+  snap:function(endValue) {
+    return Math.round(endValue / rotationSnap) * rotationSnap;
+  }
+});
+
+INPUT.addEventListener('input', onInput)
+
+onInput()
